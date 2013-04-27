62cf314b400259a20e5efdff0d8902f3e002f1fb
Hannah Fouasnon <hanelifou@gmail.com>
2013-04-26 22:01:15 -0700

    fixing route issues

--- js/app.js
+++ js/app.js
@@ -6,7 +6,7 @@ angular.module('art', []).
   config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
     $locationProvider.html5Mode(true);
 
-    $routeProvider.when('/', {
+    $routeProvider.when('/art-2.0', {
       templateUrl: 'partials/hello_world.html'
     });
     $routeProvider.otherwise({redirectTo: '/'});
